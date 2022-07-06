e71d385464b65cd95693c34f44a0db468a7e6305
Nenodias <horacio.dias92@gmail.com>
2022-07-06 00:05:08 -0300

    Creating structs for dynamic loading properties

--- config/config.go
+++ config/config.go
@@ -1,24 +1,59 @@
 package config
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
 
+type PropertySource struct {
+	Name   string         `json:"name"`
+	Source map[string]any `json:"source"`
+}
+
+type ConfigProperties struct {
+	Name            string           `json:"name"`
+	Profiles        []string         `json:"profiles"`
+	Label           string           `json:"label"`
+	Version         string           `json:"version"`
+	State           string           `json:"state"`
+	PropertySources []PropertySource `json:"propertySources"`
+}
+
+var Config = ConfigProperties{}
+
 func GetConfig(app string) error {
 	cloudURI := os.Getenv("SPRING_CLOUD_URI")
 	label := os.Getenv("SPRING_CLOUD_LABEL")
 	profiles := os.Getenv("SPRING_PROFILES_ACTIVE")
 	url := fmt.Sprintf("%s/%s/%s/%s", cloudURI, app, profiles, label)
-	res, err := http.Get(url)
+	client := http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal(err.Error())
 		return err
 	}
-	dados, err := ioutil.ReadAll(res.Body)
+	req.Header.Add("Accept", "application/json")
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err.Error())
+		return err
+	}
+	buf := &bytes.Buffer{}
+	tee := io.TeeReader(res.Body, buf)
+	jsonDecoder := json.NewDecoder(tee)
+	err = jsonDecoder.Decode(&Config)
+	if err != nil {
+		log.Fatal(err.Error())
+		return err
+	}
+
+	dados, err := ioutil.ReadAll(buf)
 	if err != nil {
 		log.Fatal(err.Error())
 		return err
--- main.go
+++ main.go
@@ -1,14 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/nenodias/go-config-client/config"
 )
 
+type Prefixable interface {
+	Prefix() string
+}
+
+type EurekaInstance struct {
+	NonSecurePort   int64 `config:"nonSecurePort"`
+	PreferIpAddress bool  `config:"preferIpAddress"`
+}
+
+func (e *EurekaInstance) Prefix() string {
+	return "instance"
+}
+
+type EurekaProperties struct {
+	Instance    EurekaInstance `config:"instance"`
+	DefaultZone string         `config:"client.serviceUrl.defaultZone"`
+}
+
+func (e *EurekaProperties) Prefix() string {
+	return "eureka"
+}
+
 func main() {
-	os.Setenv("SPRING_CLOUD_CONFIG", "http://localhost:8888")
-	os.Setenv("SPRING_CLOUD_LABEL", "default")
-	os.Setenv("SPRING_PROFILES_ACTIVE", "default")
-	config.GetConfig("greendogdelivery")
+	os.Setenv("SPRING_CLOUD_URI", "http://localhost:8888")
+	os.Setenv("SPRING_CLOUD_LABEL", "main")
+	os.Setenv("SPRING_PROFILES_ACTIVE", "default,dev")
+	config.GetConfig("test")
+
+	props := make(map[string]any)
+	for _, v := range config.Config.PropertySources {
+		for k := range v.Source {
+			if _, ok := props[k]; !ok {
+				props[k] = v.Source[k]
+			}
+		}
+	}
+
+	q := EurekaProperties{}
+	fmt.Println(q)
+
 }
